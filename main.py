b33fd8c325a242483eab81458142d3fc9e9e57ec
hriteshMaikap <hriteshkumarmaikap@gmail.com>
2025-08-27 18:54:55 +0530

    Module 2: Encapsulation

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ class Stock:
         self.ticker = ticker
         self.price = price
         self.company = company
+
+    #For encapsulation we use getter setter and set price to internal use using _price
+    @property
+    def price(self):
+        """The getter method for internal _price"""
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        """Validates the data before modification"""
+        if value<0:
+            raise ValueError("Price cannot be negative")
+        self._price=value
+
     
     def printDetails(self):
         print(f"Details for the {self.company}")
         print(f"Ticker:{self.ticker}\nPrice:{self.price}")
         print('\n')
 
+    
+
 apple = Stock(ticker="AAPL", price=150.00, company="Apple")
 google = Stock(ticker="GOOGL", price=2800.00,company="Google")
 
 apple.printDetails()
 google.printDetails()
-
